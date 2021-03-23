1ca91b2dcd1ebb0a1d5f63bc98a8be68ebb3ebc8
Jay Lewis <jr_lewis@outlook.com>
2021-03-23 21:02:00 +0000

    Added menu selection

--- src/pages/menus.tsx
+++ src/pages/menus.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, Fragment } from 'react'
 import Layout from "../components/layout/layout";
 import Seo from "../components/seo";
 import Menu from "../components/menus/Menu";
@@ -6,29 +6,53 @@ import Menu from "../components/menus/Menu";
 const breakfastMenu =  require("../../menus/food/breakfast.json");
 
 const Menus = () => {
-  const [menuSelection, setMenuSelection] = useState("");
+  const [menuSelection, setMenuSelection] = useState("breakfast");
   return (
     <Layout>
       <Seo title="menus" />
     <div className="menus-container">
-      <h3>Change the menu selection</h3>
-      <span className="button-wrapper">
-          <button onClick={() => setMenuSelection("food")} >
+      <span className="menu-selection">
+          <button 
+            className={menuSelection === "breakfast" ? "selected" : ""}
+            onClick={() => setMenuSelection("breakfast")} >
             <span className="btn-bg"></span>
-            <p>Food</p>
+            <p>Breakfast</p>
           </button>
-          <button onClick={() => setMenuSelection("drinks")} >
+          <button 
+            className={menuSelection === "main" ? "selected" : ""}
+            onClick={() => setMenuSelection("main")} >
+          <span className="btn-bg"></span>
+            <p>Main Menu</p>
+          </button>
+          <button
+            className={menuSelection === "drinks" ? "selected" : ""}
+            onClick={() => setMenuSelection("drinks")} >
           <span className="btn-bg"></span>
             <p>Drinks</p>
           </button>
       </span>
-      <div className="menu-selection-wrapper">
+      {/* <div className="menu-selection-wrapper">
         { menuSelection === "food" && <p>Food Menus</p> }
         { menuSelection === "drinks" && <p>Drinks Menus</p> }
-      </div>
-      {breakfastMenu.map((m: any) => (
+      </div> */}
+      {  menuSelection === "breakfast" &&  
+      <Fragment>   
+        {breakfastMenu.map((m: any) => (
         <Menu key={m.key} name={m.name} sections={m.sections} />
-      ))}
+       ))}
+      </Fragment> 
+      }
+      {  menuSelection === "main" &&  
+        <div className="menu-wrapper">
+          <h2 className="menu-name">Main Menu</h2>
+        </div>
+      }
+      {  menuSelection === "drinks" &&  
+        <div className="menu-wrapper">
+          <h2 className="menu-name">Drinks</h2>
+        </div>
+      }
+ 
     </div>
     </Layout>
   )
